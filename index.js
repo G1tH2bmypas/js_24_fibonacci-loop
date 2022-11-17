ebd800443e93c7c70f2244b31712562205222018
Tekyto <Tekyto2014@gmail.com>
2022-11-17 21:06:30 +0200

    js_24_fibonacci-loop???

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function fibonacci(n) {
-	// write your code here
+	let a = 0;
+	let b = 0;
+	let c = 1;
+
+	if (n === 1) {
+		return 0;
+	} else if (n === 2) {
+		return 1;
+	}
+
+	for ( let i = 2; i < n ; i++) {
+		b = a;
+		a = c;
+		c = a + b;
+	}
+
+	return c;
 }
 
 module.exports = fibonacci;
